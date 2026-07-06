4cffa08fe995347fe5efdf411091172f140669bc
fkfkfk112233 <kk60607kk@gmail.com>
2026-07-06 14:38:43 +0800

    Create FoodService.java

---
+++ src/main/java/service/FoodService.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.util.List;
+
+import model.Food;
+
+/**
+ * Food 業務邏輯介面
+ */
+public interface FoodService {
+
+    /** 新增餐點 */
+    void addFood(Food food);
+
+    /** 更新餐點 */
+    void updateFood(Food food);
+
+    /** 刪除餐點 */
+    void deleteFood(int id);
+
+    /** 取得全部餐點清單 */
+    List<Food> getAllFoods();
+
+    /** 依類型取得餐點清單 */
+    List<Food> getFoodsByType(String type);
+
+    /**
+     * 隨機選出一筆餐點
+     *
+     * @param type 篩選類型；若為 null 或 "全部" 則從所有餐點中隨機挑選
+     * @return 隨機選中的 Food；若清單為空則回傳 null
+     */
+    Food getRandomFood(String type);
+}
